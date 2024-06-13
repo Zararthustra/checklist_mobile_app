4a88f82dba62255e4f7fe3fce29a19ae7c76a9b8
Zararthustra <70105093+Zararthustra@users.noreply.github.com>
2024-06-13 16:33:15 +0200

    feat(Animation): add scroll snap animation

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -1,5 +1,14 @@
 import { ICategory, ITask } from "@interfaces/index";
-import { FlatList, Text, View, StyleSheet, Pressable } from "react-native";
+import {
+  FlatList,
+  Text,
+  View,
+  StyleSheet,
+  Pressable,
+  ScrollView,
+  Dimensions,
+  Animated,
+} from "react-native";
 import { Task } from "./Task";
 import { IconHidden, IconOptions, IconVisible } from "@assets/index";
 import { OptionsModal } from "./modals/OptionsModal";
@@ -8,9 +17,20 @@ import { useState } from "react";
 interface ICategoryProps {
   category: ICategory;
   tasks: ITask[] | undefined;
+  index: number;
+  CATEGORY_HEIGHT: number;
+  CATEGORY_WIDTH: number;
+  scrollX: Animated.Value;
 }
 
-export const Category = ({ category, tasks }: ICategoryProps) => {
+export const Category = ({
+  category,
+  tasks,
+  index,
+  scrollX,
+  CATEGORY_HEIGHT,
+  CATEGORY_WIDTH,
+}: ICategoryProps) => {
   const [showModal, setShowModal] = useState<boolean>(false);
   const dynamicStyle = StyleSheet.create({
     header: {
@@ -18,37 +38,78 @@ export const Category = ({ category, tasks }: ICategoryProps) => {
     },
   });
 
+  const { width } = Dimensions.get("window");
+  const categoriesGap = 5;
+  const inputRange = [
+    (index - 2) * CATEGORY_WIDTH,
+    (index - 1) * CATEGORY_WIDTH,
+    index * CATEGORY_WIDTH,
+  ];
+
+  if (["left", "right"].includes(category.name)) {
+    return (
+      <View
+        style={{ width: (width - CATEGORY_WIDTH) / 2 - categoriesGap * 2 }}
+      />
+    );
+  }
+
   return (
-    <View className="">
+    <Animated.View
+      style={{
+        width: CATEGORY_WIDTH - categoriesGap * 2,
+        height: CATEGORY_HEIGHT,
+        marginHorizontal: categoriesGap,
+        transform: [
+          {
+            translateY: scrollX.interpolate({
+              inputRange,
+              outputRange: [100, 0, 100],
+            }),
+          },
+        ],
+        opacity: scrollX.interpolate({
+          inputRange,
+          outputRange: [0.2, 1, 0.2],
+        }),
+      }}
+    >
       <OptionsModal
         category={category}
         showModal={showModal}
         setShowModal={setShowModal}
       />
 
-      {/* Header */}
-      <View
-        style={dynamicStyle.header}
-        className="flex-row justify-between items-center px-5 py-2 rounded-tl-xl"
-      >
-        {category.isHidden ? (
-          <IconHidden className="text-zinc-800" />
-        ) : (
-          <IconVisible className="text-zinc-800" />
-        )}
-        <Text className="text-2xl font-bold">{category.name}</Text>
-        <Pressable onPress={() => setShowModal(!showModal)}>
-          <IconOptions className="text-zinc-800" />
-        </Pressable>
-      </View>
+      <View style={{ maxHeight: CATEGORY_HEIGHT }}>
+        {/* Header */}
+        <View
+          style={dynamicStyle.header}
+          className="flex-row justify-between items-center px-5 py-2 rounded-tl-xl"
+        >
+          {/* 
+          {category.isHidden ? (
+            <IconHidden className="text-zinc-800" />
+          ) : (
+            <IconVisible className="text-zinc-800" />
+          )}
+           */}
+          <View className="w-[24px]"></View>
+          <Text className="text-2xl font-bold">{category.name}</Text>
+          <Pressable onPress={() => setShowModal(!showModal)}>
+            <IconOptions className="text-zinc-800" />
+          </Pressable>
+        </View>
 
-      {/* Tasks */}
-      {tasks && tasks.map((task) => <Task key={task.id} task={task} />)}
+        {/* Tasks */}
+        <ScrollView>
+          {tasks && tasks.map((task) => <Task key={task.id} task={task} />)}
+        </ScrollView>
 
-      {/* Footer */}
-      <View style={dynamicStyle.header} className="py-2 rounded-br-xl">
-        <Text className=""></Text>
+        {/* Footer */}
+        <View style={dynamicStyle.header} className="py-2 rounded-br-xl">
+          <Text className=""></Text>
+        </View>
       </View>
-    </View>
+    </Animated.View>
   );
 };
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,17 @@
-import { useCallback } from "react";
-import { FlatList, Text, View } from "react-native";
+import { useCallback, useRef } from "react";
+import { Animated, Dimensions, FlatList, Text, View } from "react-native";
 import {
   useQueryRetrieveCategories,
   useQueryRetrieveTasks,
 } from "@queries/index";
 import { Category, Header } from "@components/index";
 import { ICategory } from "@interfaces/index";
+
+// Globals
+const { width, height } = Dimensions.get("window");
+
+const CATEGORY_HEIGHT = height * 0.82;
+const CATEGORY_WIDTH = width > 400 ? width * 0.55 : width * 0.85;
 
 export const Home = () => {
   const {
@@ -18,15 +24,20 @@ export const Home = () => {
     error: errorTasks,
     isLoading: loadingTasks,
   } = useQueryRetrieveTasks();
+  const scrollX = useRef(new Animated.Value(0)).current;
 
   // Using an arrow function inline on Flatlist
   // re-creates the function on every re-render
   // causing performance issue => better useCallback
   const renderItem = useCallback(
-    ({ item }: any) => (
+    ({ item, index }: any) => (
       <Category
         category={item}
         tasks={tasks?.filter((task) => item.id === task.category)}
+        index={index}
+        scrollX={scrollX}
+        CATEGORY_HEIGHT={CATEGORY_HEIGHT}
+        CATEGORY_WIDTH={CATEGORY_WIDTH}
       />
     ),
     [tasks]
@@ -50,14 +61,46 @@ export const Home = () => {
     );
 
   return (
-    <View className="flex-1 p-3 dark:bg-zinc-900">
+    <View className="p-3 dark:bg-zinc-900">
       <Header />
-      <FlatList
-        contentContainerStyle={{ gap: 10 }}
-        initialNumToRender={2}
-        data={categories}
+      <Animated.FlatList
+        // initialNumToRender={2}
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+          { useNativeDriver: true }
+        )}
+        data={[
+          {
+            id: "left",
+            name: "left",
+            color: "string",
+            isHidden: false,
+            isRecurrent: false,
+            owner: 1,
+          },
+          ...(categories as ICategory[]),
+          {
+            id: "right",
+            name: "right",
+            color: "string",
+            isHidden: false,
+            isRecurrent: false,
+            owner: 1,
+          },
+        ]}
         renderItem={renderItem}
         keyExtractor={keyExtractor}
+        contentContainerStyle={
+          {
+            // alignItems: "center",
+          }
+        }
+        horizontal
+        snapToInterval={CATEGORY_WIDTH}
+        decelerationRate={"fast"}
+        scrollEventThrottle={16}
+        bounces={false}
+        showsHorizontalScrollIndicator={false}
       />
     </View>
   );
