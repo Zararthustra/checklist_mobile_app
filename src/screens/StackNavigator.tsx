bf1315a7b851929b4d6e81856ddf7d33af8f6799
Zararthustra <70105093+Zararthustra@users.noreply.github.com>
2024-08-28 22:11:33 +0200

    fix: status bar was transparent

--- src/screens/StackNavigator.tsx
+++ src/screens/StackNavigator.tsx
@@ -4,6 +4,8 @@ import { Home } from "./Home";
 import { Login } from "./Login";
 import { SplashScreen } from "./SplashScreen";
 import { Register } from "./Register";
+import { StatusBar } from "react-native";
+import { useColorScheme } from "nativewind";
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -16,10 +18,18 @@ function StackNavigator({
     accessToken: string | null;
   };
 }) {
+  const { colorScheme } = useColorScheme();
+  const isDarkMode = colorScheme === "dark";
+
   if (state.isLoading) return <SplashScreen />;
 
   return (
     <NavigationContainer>
+      <StatusBar
+        animated={true}
+        backgroundColor={isDarkMode ? "#18181b" : "#f4f4f5"}
+        barStyle={isDarkMode ? "light-content" : "dark-content"}
+      />
       <Navigator>
         {state.accessToken == null ? (
           <>
